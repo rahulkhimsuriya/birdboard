1542329332f099da9dd3cbeee439ed83d71bdfd6
rahulkhimsuriya <rahulkhimsuriya38@gmail.com>
2020-03-22 15:31:19 +0530

    modify views ✨

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,19 +2,20 @@
 
 @section('content')
 
-<div class="d-flex align-items-center">
-  <h1>Birdboard</h1>
-  <a href="/projects/create" class="ml-auto">Create New Project</a>
+<div class="flex items-center my-5">
+  <h4 class="text-2xl">Birdboard</h4>
+  <a href="/projects/create" class="ml-auto">Create Project</a>
 </div>
 
-<ul>
+<div class="flex">
   @forelse ($projects as $project)
-    <li>
-      <a href="{{ $project->path() }}">{{ $project->title }}</a>
-    </li>
+    <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+      <h3 class="text-xl font-normal py-4">{{ $project->title }}</h3>
+      <div class="text-gray-500">{{ Str::limit($project->description) }}</div>
+    </div>
   @empty
-    <li>No projects yet.</li>
+      <div>No projects yet.</div>
   @endforelse
-</ul>
+</div>
 
 @endsection
