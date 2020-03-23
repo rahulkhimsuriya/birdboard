2326e25569c71f16d03830dee5dc4234a863ba3f
rahulkhimsuriya <rahulkhimsuriya38@gmail.com>
2020-03-23 12:17:54 +0530

    projects show DESC order 👁

--- app/Task.php
+++ app/Task.php
@@ -4,17 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
-{
-    protected $guarded = [];
-
-    public function path()
-    {
-        return "/projects/{$this->project->id}/tasks/{$this->id}";
-    }
-
-    public function project()
-    {
-        return $this->belongsTo(Project::class);
-    }
+class Task extends Model {
+  
+  protected $guarded = [];
+  
+  protected $touches = [ 'project' ];
+  
+  public function path()
+  {
+    return "/projects/{$this->project->id}/tasks/{$this->id}";
+  }
+  
+  public function project()
+  {
+    return $this->belongsTo( Project::class );
+  }
+  
 }
