bdacfd3a758686372301e749a144b9ff00673b60
rahulkhimsuriya <rahulkhimsuriya38@gmail.com>
2020-03-23 14:54:07 +0530

    added notes 📓 to the project and policy 🐱‍ with test

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,42 +1,53 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Project;
-use Illuminate\Http\Request;
-
-class ProjectsController extends Controller
-{
-    public function index()
+  
+  namespace App\Http\Controllers;
+  
+  use App\Project;
+  use Illuminate\Http\Request;
+  
+  class ProjectsController extends Controller {
+    
+    public function index ()
     {
-        $projects = auth()->user()->projects;
-
-        return view('projects.index', compact('projects'));
+      $projects = auth()->user()->projects;
+      
+      return view( 'projects.index', compact( 'projects' ) );
     }
-
-    public function show(Project $project)
+    
+    public function show ( Project $project )
     {
-        if (auth()->user()->isNot($project->owner)) {
-            abort(403);
-        }
-
-        return view('projects.show', compact('project'));
+      $this->authorize( 'update', $project );
+      
+      return view( 'projects.show', compact( 'project' ) );
     }
-
-    public function create()
+    
+    public function create ()
     {
-        return view('projects.create');
+      return view( 'projects.create' );
     }
-
-    public function store()
+    
+    public function store ()
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'description' => 'required'
-        ]);
-
-        $project = auth()->user()->projects()->create($attributes);
-
-        return redirect($project->path());
+      $attributes = request()->validate( [
+        'title' => 'required',
+        'description' => 'required',
+        'notes' => [ 'min:3', 'max:255' ],
+      ] );
+      
+      $project = auth()->user()->projects()->create( $attributes );
+      
+      return redirect( $project->path() );
     }
-}
+    
+    public function update ( Project $project )
+    {
+      $this->authorize( 'update', $project );
+      
+      $project->update( [
+        'notes' => request( 'notes' ),
+      ] );
+      
+      return redirect( $project->path() );
+    }
+    
+  }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,60 +2,70 @@
 
 @section('content')
 
-<header class="flex items-end my-6">
-  <h2 class="text-xl font-normal text-gray-500">
-    <a href="/projects" class="text-xl font-normal text-gray-500">My Projects</a> / {{ $project->title }}
-  </h2>
-  <a href="/projects/create" class="ml-auto button">Create Project</a>
-</header>
+  <header class="flex items-end my-6">
+    <h2 class="text-xl font-normal text-gray-500">
+      <a href="/projects" class="text-xl font-normal text-gray-500">My Projects</a> / {{ $project->title }}
+    </h2>
+    <a href="/projects/create" class="ml-auto button">Create Project</a>
+  </header>
 
-<main>
-  <div class="lg:flex -mx-3">
+  <main>
+    <div class="lg:flex -mx-3">
 
-    <div class="lg:w-3/4 px-3 mb-6 lg:mb-0">
+      <div class="lg:w-3/4 px-3 mb-6 lg:mb-0">
 
-      <div class="mb-8">
-        <h2 class="text-lg mb-3 font-normal text-gray-500">Tasks</h2>
-        {{-- Tasks --}}
-        @foreach ($project->tasks as $task)
-          <div class="card mb-3">
+        <div class="mb-8">
+          <h2 class="text-lg mb-3 font-normal text-gray-500">Tasks</h2>
+          {{-- Tasks --}}
+          @foreach ($project->tasks as $task)
+            <div class="card mb-3">
 
-          <form action="{{ $task->path() }}" method="post">
+              <form action="{{ $task->path() }}" method="post">
+                @csrf
+                @method('PATCH')
+                <div class="flex">
+                  <input type="text" name="body" value="{{ $task->body }}"
+                         class="w-full {{ $task->completed ? 'text-gray-500' : '' }}">
+                  <input type="checkbox" name="completed" id="completed"
+                         onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                </div>
+              </form>
+
+            </div>
+          @endforeach
+
+          <div class="card">
+            <form action="{{ $project->path().'/tasks' }}" method="post">
+              @csrf
+              <input type="text" placeholder="Add a new task..." name="body" class="w-full">
+            </form>
+          </div>
+
+        </div>
+
+        <div class="mb-8 lg:mb-0">
+          <h2 class="text-lg mb-3 font-normal text-gray-500">General Notes</h2>
+          {{-- General Notes --}}
+          <form action="{{ $project->path() }}" method="post">
             @csrf
             @method('PATCH')
-            <div class="flex">
-              <input type="text" name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-gray-500' : '' }}">
-              <input type="checkbox" name="completed" id="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
-            </div>
-          </form>
 
-        </div>
-        @endforeach
+            <textarea class="card w-full mb-4" name="notes" id="notes" rows="5"
+                      placeholder="Anything special that you want to make a note of?">{{ $project->notes  }}</textarea>
 
-        <div class="card">
-          <form action="{{ $project->path().'/tasks' }}" method="post">
-          @csrf
-            <input type="text" placeholder="Add a new task..." name="body" class="w-full">
+            <button type="submit" class="button">Save</button>
+
           </form>
         </div>
 
       </div>
 
-      <div class="mb-8 lg:mb-0">
-        <h2 class="text-lg mb-3 font-normal text-gray-500">General Notes</h2>
-        {{-- General Notes --}}
-        <textarea class="card w-full" name="" id="" rows="5">Lorem ipsum.</textarea>
-        {{-- <div class="card">Lorem ipsum.</div> --}}
+      <div class="lg:w-1/4 px-3 mt-8">
+
+        @include('projects._card')
+
       </div>
-
     </div>
-
-    <div class="lg:w-1/4 px-3">
-
-      @include('projects._card')
-
-    </div>
-  </div>
-</main>
+  </main>
 
 @endsection
